7126ece2dacb3a03b5cc3bc685512abe9f92a612
Juan Antonio Fajardo Serrano <juan61088@gmail.com>
2024-03-09 15:57:39 +0100

    Added SharedAssemblyInfo

---
+++ Core/SharedAssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyProduct("Sysmac Studio Parameter Editor User Selection File Maker")]
+[assembly: AssemblyCompany("JAFS6")]
+[assembly: AssemblyCopyright("Copyright © 2024 JAFS6")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
