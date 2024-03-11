9048570940015e9fb67e67aa196e1b8dbb0a9eae
Juan Antonio Fajardo Serrano <juan61088@gmail.com>
2024-03-11 12:47:56 +0100

    Updated Assembly version and CHANGELOG

--- Core/SharedAssemblyInfo.cs
+++ Core/SharedAssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyProduct("Sysmac Studio Parameter Editor User Selection File Maker")]
 [assembly: AssemblyCompany("JAFS6")]
